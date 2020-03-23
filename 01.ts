01f74418b5101499ba4b751b7e981a7a5ca4dbf6
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-23 14:08:51 +0700

    01 palindrome

---
+++ 01.ts
@@ -0,0 +1,30 @@
+/* PALINDROME */
+
+export {}
+
+const sentences: string = `
+ibu ratna antar ubi
+
+kasur ini rusak
+
+A nut for a jar of tuna.
+
+Borrow or rob?
+
+Was it a car or a cat I saw?
+
+Yo, banana boy!
+
+UFO tofu?
+`;
+
+let separate = s => s.split(/[\s?!]+/).slice(1,-1);
+let arrayTes: string[] = separate(sentences);
+let palindrome = s => separate(s).forEach((v,i) => Boolean(v == v.split("").reverse().join("")) ? console.log(v + ' = Palindrome ') : '');
+
+
+let reverse = s => s.split("").reverse().join("");
+console.log('Input : palindrome(sentences)');
+console.log('=============================');
+console.log('Output : \\/');
+palindrome(sentences);
